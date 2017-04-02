c8728566962ec9431d9db9f8747aa57189c7cca1
dietlera15 <andreas.dietler@edu.fh-joanneum.at>
2017-04-02 17:22:46 +0200

    added new projects

---
+++ WebSpra_20160514_autocomplete/autocomplete.js
@@ -0,0 +1,10 @@
+/**
+ * autocomplete.js
+ * 
+ */
+
+function autocompleteHandler() {
+	var element = document.getElementById("autocomplete_name");
+	var text = element.value;
+	//console.log("Eingabe: " + text);
+}
